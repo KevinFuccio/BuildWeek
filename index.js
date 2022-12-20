b705b1e164080d96c5c21b1942029db11adaa7b8
MichelaKalowski <mi.ky90@hotmail.com>
2022-12-20 16:57:40 +0100

    aggiornamento

---
+++ index.js
@@ -0,0 +1,12 @@
+
+document.getElementById("myBtn").disabled = true;
+const checkbox = document.querySelector("input[name=checkbox]");
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+      console.log("Checkbox is checked..");
+      document.getElementById("myBtn").disabled = false;
+    } else {
+      console.log("Checkbox is not checked..");
+      document.getElementById("myBtn").disabled = true;
+    }
+  });
