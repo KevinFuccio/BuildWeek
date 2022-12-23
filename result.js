486b666e8a5010c93c669dda78b62e490a394169
kevin-fuccio <kevin.fuccio90@gmail.com>
2022-12-23 17:04:02 +0100

    aa

--- result.js
+++ result.js
@@ -11,6 +11,29 @@ risultatoSbagliato.innerText = `${(sbagliata / maxcorrect) *100}%`
 let totalQuestionsWrong = document.getElementById("totalQuestionsWrong")
 totalQuestionsWrong.innerText= `${sbagliata}/${maxcorrect} Questions`
 
+
+if(pcorrect > 5){
+  let content = document.querySelector(".content")
+  let h3 = document.createElement("h3")
+  let span = document.createElement("span")
+  span.innerText= "You passed the exam."
+  span.classList.add("content")
+  h3.innerText = "Congratulations!"
+  content.appendChild(h3)
+  content.appendChild(span)
+}else{
+  let content = document.querySelector(".content")
+  let h3 = document.createElement("h3")
+  let span = document.createElement("span")
+  span.innerText= "You didn't pass you're bad"
+  span.classList.add("content")
+  h3.innerText = "Congratulations!"
+  content.appendChild(h3)
+  content.appendChild(span)
+}
+
+
+
 document.querySelector(".btn").addEventListener('click',nextPage)
   function nextPage(btn){
     btn = location.assign(`/feedback.html?=pagina4`)
